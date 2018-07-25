ca88fcc7a85b4d459344dfc893fe1d29511ff2a6
Matt Roberts <code@rbrts.uk>
2018-07-25 12:18:08 +0100

    Inital commit

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # webpack-composer
+
+A minimal sample showing the composer-common library running in the browser inside a React app.
+
+Run locally with 
+
+```
+npm install
+npm run start
+```
+
+Go to http://localhost:8080 and see the list of namespaces in the ModelManager including the custom model file that was added within the React app.
---
+++ src/index.js
@@ -0,0 +1,24 @@
+
+import React from "react";
+import ReactDOM from "react-dom";
+
+const ModelManager = require('composer-common').ModelManager;
+
+const mm = new ModelManager();
+mm.addModelFile(`
+namespace matt.test
+
+concept Matt {
+  o String name
+}
+`, 'test.cto', true);
+
+const listItems = mm.getNamespaces().map((n) =>
+  <li>{n}</li>
+);
+
+const Index = () => {
+    return <li>{listItems}</li>;
+};
+
+ReactDOM.render(<Index />, document.getElementById("index"));
